c08277792da06d001fcfeb4ca91f00ba49a4129a
Anas Alfaihan <narotoaozomake99@gmail.com>
2024-11-02 19:00:27 +0300

    super user

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { ValidateEnv } from '@utils/validateEnv';
 import { CourseRoute } from './routes/course.routes';
 import { lectureRoute } from './routes/lecture.routes';
 import { QuestionRoute } from './routes/question.routes';
-import { AnswerRoute } from './routes/answer.routes';
 import { UserCoursesMapping } from './routes/user-courses-mapping.routes';
 import { UserLecturesMapping } from './routes/user-lecture-mapping.routes';
 import { AuthRoute } from './routes/auth.routes';
@@ -16,7 +15,6 @@ const app = new App([
   new CourseRoute(),
   new lectureRoute(),
   new QuestionRoute(),
-  new AnswerRoute(),
   new UserCoursesMapping(),
   new UserLecturesMapping(),
   new AuthRoute(),
