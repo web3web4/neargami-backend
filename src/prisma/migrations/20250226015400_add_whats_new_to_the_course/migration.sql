0d400b2e494aeb56fce1e3bdc9420f8ecf5e03e6
hassan ibrahim <h.ibrahim@con3x.com>
2025-03-01 09:59:39 +0000

    whats_new

--- src/prisma/migrations/20250226015400_add_whats_new_to_the_course/migration.sql
+++ src/prisma/migrations/20250226015400_add_whats_new_to_the_course/migration.sql
@@ -1,2 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Log` table. If the table is not empty, all the data it contains will be lost.
+
+*/
 -- AlterTable
-ALTER TABLE "Course" ALTER COLUMN "whats_new" SET DATA TYPE TEXT;
+ALTER TABLE "Course" ADD COLUMN     "whats_new" TEXT;
+
+-- DropTable
+DROP TABLE "Log";
