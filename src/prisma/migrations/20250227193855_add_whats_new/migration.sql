fe9be3533c99b313f67c46c8565b1231c7639855
hassan ibrahim <h.ibrahim@con3x.com>
2025-03-01 10:01:40 +0000

    no

--- src/prisma/migrations/20250227193855_add_whats_new/migration.sql
+++ src/prisma/migrations/20250227193855_add_whats_new/migration.sql
@@ -1,11 +1,1 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Log` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Course" ADD COLUMN     "whats_new" TEXT;
-
--- DropTable
-DROP TABLE "Log";
+ 
