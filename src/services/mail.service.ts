19fb69ceafc4a101dd025f846f6c54af670b1d02
Anas Alfaihan <narotoaozomake99@gmail.com>
2025-05-18 16:17:07 +0300

    fix emails

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -9,10 +9,11 @@ export class MailService {
   private transporter: nodemailer.Transporter;
 
   constructor() {
+    console.log(EMAIL_NAME, EMAIL_PASSWORD);
     this.transporter = nodemailer.createTransport({
-      host: 'smtp-relay.brevo.com',
-      port: 587,
-      secure: false,
+      host: 'smtp.zoho.com',
+      port: 465,
+      secure: true,
       auth: {
         user: EMAIL_NAME,
         pass: EMAIL_PASSWORD,
@@ -21,48 +22,55 @@ export class MailService {
   }
 
   async sendEmail(to: string, subject: string, templateName: string, data: any): Promise<void> {
-    if (!to) {
-      throw new Error('Missing recipient email address');
-    }
-
-    if (!subject) {
-      throw new Error('Missing email subject');
-    }
-
-    if (!templateName) {
-      throw new Error('Missing email template name');
-    }
-
-    if (!data) {
-      throw new Error('Missing email template data');
-    }
-
-    const templateSource = fs.readFileSync(`../templates/${templateName}.hbs`, 'utf8');
-
-    if (!templateSource) {
-      throw new Error(`Missing template file: ${templateName}.hbs`);
-    }
-
-    const template = handlebars.compile(templateSource);
-
-    if (!template) {
-      throw new Error('Failed to compile email template');
-    }
-
-    const htmlContent = template(data);
-    const mailOptions = {
-      from: EMAIL_NAME,
-      to: to,
-      subject: subject,
-      html: htmlContent,
-    };
-
     try {
-      const info = await this.transporter.sendMail(mailOptions);
-      console.log('Message sent: %s', info.messageId);
+      if (!to) {
+        throw new Error('Missing recipient email address');
+      }
+
+      if (!subject) {
+        throw new Error('Missing email subject');
+      }
+
+      if (!templateName) {
+        throw new Error('Missing email template name');
+      }
+
+      if (!data) {
+        throw new Error('Missing email template data');
+      }
+      if (!to) {
+        throw new Error('Missing recipient email address');
+      }
+
+      const templateSource = fs.readFileSync(`./src/templates/${templateName}.hbs`, 'utf8');
+
+      if (!templateSource) {
+        throw new Error(`Missing template file: ${templateName}.hbs`);
+      }
+
+      const template = handlebars.compile(templateSource);
+
+      if (!template) {
+        throw new Error('Failed to compile email template');
+      }
+
+      const htmlContent = template(data);
+      const mailOptions = {
+        from: EMAIL_NAME,
+        to: to,
+        subject: subject,
+        html: htmlContent,
+      };
+
+      try {
+        const info = await this.transporter.sendMail(mailOptions);
+        console.log('Message sent: %s', info.messageId);
+      } catch (error) {
+        console.error('Error occurred:', error);
+        throw error; // Re-throw the error for the caller to handle if needed
+      }
     } catch (error) {
-      console.error('Error occurred:', error);
-      throw error; // Re-throw the error for the caller to handle if needed
+      throw error;
     }
   }
 }
