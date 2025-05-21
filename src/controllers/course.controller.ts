9bb0adcb8ef98effa334b05e8502d85f4feb0d56
hassan <yazanaloali@gmail.com>
2025-05-20 17:26:11 -0700

    get the last version on get all course by status

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -90,7 +90,7 @@ export class CourseController {
     try {
       const { id } = req.params;
 
-      const courses: Course[] = await this.courseService.findAllCoursesByStatus(id as Status);
+      const courses: Course[] = await this.courseService.findAllCoursesByStatusLastestVersion(id as Status);
       res.status(200).json({ data: courses, message: 'findAll' });
     } catch (error) {
       next(error);
@@ -399,6 +399,7 @@ export class CourseController {
       next(error);
     }
   };
+
   // find all courses was finished by student (name)
   public findCompletedCoursesByStudentName = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
